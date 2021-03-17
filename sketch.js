316b5d1d3e9e2c328edb8e8defb1a3c0577c7964
rachit-patel <74806518+rachit-patel@users.noreply.github.com>
2021-03-17 14:52:14 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,54 @@
+var ball;
+var position,database;
+
+function setup(){
+    database=firebase.database
+    console.log(database)
+    createCanvas(500,500);
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+    var BallPosition =database.ref('Ball/Position')
+    BallPosition.on("value",readPosition,showError)
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        changePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        changePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        changePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        changePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function changePosition(x,y){
+    ball.x = ball.x + x;
+    ball.y = ball.y + y;
+}
+function writePosition(x,y){
+database.ref('Ball/Position').set({
+    'x':position.x+x,
+    'y':position.y+y
+})
+
+}
+function readPosition(data){
+position=data.val()
+console.log(position.x)
+Ball.x=position.x
+Ball.y=position.y
+
+
+}
+function showError(){
+console.log("error")
+
+
+}
